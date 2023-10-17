75376a9d9196717e0b0a80aa3035605e51f8940b
armangracia <graciaarmansteven@gmail.com>
2023-10-17 08:22:06 +0800

    update

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './Components/Layout/Header'
+import Footer from './Components/Layout/Footer'
+import Home from './Components/Home'
+import ProductDetails from './Components/Product/ProductDetails'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} exact="true" />
+          <Route path="/product/:id" element={<ProductDetails />} exact="true" />
+          <Route path="/search/:keyword" element={<Home />} exact="true" />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
